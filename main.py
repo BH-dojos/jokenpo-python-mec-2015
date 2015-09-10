8470c5be49cc74557f0f3c18f731243b8f071ecd
Dojo People <dojo@thoughtworks.com>
2015-09-10 11:06:57 -0300

    Refatoração da estrutura do vencedor

--- main.py
+++ main.py
@@ -5,7 +5,22 @@ class Rodada:
 		self.jogador2 = jogador2
 
 	def vencedor (self):
-		return "Papel"
+		dicionario = {
+			"Pedra":{
+				"Tesoura" : "Pedra",
+				"Papel" : "Papel"
+			},
+			"Tesoura": {
+				"Pedra": "Pedra",
+				"Papel": "Tesoura"
+			},
+			"Papel": {
+				"Pedra": "Papel",
+				"Tesoura": "Tesoura"
+			}
+		}
+
+		return dicionario[self.jogador1][self.jogador2]
 
 
 def main():
--- test_main.py
+++ test_main.py
@@ -8,7 +8,28 @@ class TestDojo(unittest.TestCase):
 
     def test_pedra_perde_do_papel(self):
         rodada = Rodada("Pedra", "Papel")
-        assert rodada.vencedor() == "Papel"
+        self.assertEqual("Papel", rodada.vencedor())
+
+    def test_papel_ganha_da_pedra(self):
+        rodada = Rodada("Papel", "Pedra")
+        self.assertEqual("Papel", rodada.vencedor())
+
+    def test_pedra_ganha_de_tesoura(self):
+        rodada = Rodada("Pedra", "Tesoura")
+        self.assertEqual("Pedra", rodada.vencedor())
+
+    def test_tesoura_perde_de_pedra(self):
+        rodada = Rodada("Tesoura", "Pedra")
+        self.assertEqual("Pedra", rodada.vencedor())
+
+    def test_tesoura_ganha_de_papel(self):
+        rodada = Rodada("Tesoura", "Papel")
+        self.assertEqual("Tesoura", rodada.vencedor())
+
+    def test_papel_perde_de_tesoura(self):
+        rodada = Rodada("Papel", "Tesoura")
+        self.assertEqual("Tesoura", rodada.vencedor())
+
 
 if __name__ == '__main__':
     unittest.main()
