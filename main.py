74ccdcf0fd6ca309b7df40c49f20500bb34bb915
Dojo People <dojo@thoughtworks.com>
2015-09-10 10:26:13 -0300

    Validate test

---
+++ main.py
@@ -0,0 +1,15 @@
+class Rodada:
+
+	def __init__(self, jogador1, jogador2):
+		self.jogador1 = jogador1
+		self.jogador2 = jogador2
+
+	def vencedor (self):
+		return "Papel"
+
+
+def main():
+	print "digite a entrada: "
+
+if __name__ == '__main__':
+	main()
--- test_main.py
+++ test_main.py
@@ -1,13 +1,14 @@
 import unittest
-from main
+from main import Rodada
 
 class TestDojo(unittest.TestCase):
 
     def setUp(self):
         pass
 
-    def tearDown(self):
-        pass
+    def test_pedra_perde_do_papel(self):
+        rodada = Rodada("Pedra", "Papel")
+        assert rodada.vencedor() == "Papel"
 
-if __name__ == 'main':
+if __name__ == '__main__':
     unittest.main()
