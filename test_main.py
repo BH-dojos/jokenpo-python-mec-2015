d99d5b32b29b5e559461540c02b585eb3d7536ed
Dojo People <dojo@thoughtworks.com>
2015-09-10 10:12:24 -0300

    Initial Import

---
+++ test_main.py
@@ -0,0 +1,13 @@
+import unittest
+from main
+
+class TestDojo(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == 'main':
+    unittest.main()
